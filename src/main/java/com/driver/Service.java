ae5ef7f10aead4b77c414c7be54102a7247cfb82
Aman <ankesh.aman@accenture.com>
2022-11-02 15:23:41 +0530

    commited

--- src/main/java/com/driver/Service.java
+++ src/main/java/com/driver/Service.java
@@ -1,11 +1,17 @@
 package com.driver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.driver.repository.Repo;
+
 @org.springframework.stereotype.Service
 public class Service {
-
+	@Autowired
+	Repo rep;
+	
 	public Book getBookbyId(int id) {
 		// TODO Auto-generated method stub
-		Book b=new Book();
+		Book b=rep.getBookbyId(id);
 		return b;
 	}
 
---
+++ src/main/java/com/driver/repository/Repo.java
@@ -0,0 +1,16 @@
+package com.driver.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.driver.Book;
+
+@Repository
+public class Repo {
+
+	public Book getBookbyId(int id) {
+		// TODO Auto-generated method stub
+		Book b=new Book();
+		return b;
+	}
+	
+}
